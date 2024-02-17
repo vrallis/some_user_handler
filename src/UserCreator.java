a022d19138dc56659af8f7bf2ec88f827638bbab
I6356800 <v.rallis@student.maastrichtuniversity.nl>
2024-02-17 19:27:23 +0100

    Updated password generator to make more consistant password so less of them are invalid. Updated database of names that can be used to create usernames to include female names

--- src/UserCreator.java
+++ src/UserCreator.java
@@ -30,23 +30,39 @@ public class UserCreator {
     }
 
     public String generatePassword(int length) {
-        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+        String specialCharacters = "!@#$%^&*()";
+        String upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+        String numbers = "0123456789";
+    
         StringBuilder password = new StringBuilder();
         Random random = new Random();
-
-        for (int i = 0; i < length; i++) {
-            int index = random.nextInt(characters.length());
-            password.append(characters.charAt(index));
+    
+        password.append(specialCharacters.charAt(random.nextInt(specialCharacters.length())));
+        password.append(upperCaseLetters.charAt(random.nextInt(upperCaseLetters.length())));
+        password.append(numbers.charAt(random.nextInt(numbers.length())));
+    
+        String allPossibleCharacters = specialCharacters + upperCaseLetters + lowerCaseLetters + numbers;
+        for (int i = 3; i < length; i++) {
+            password.append(allPossibleCharacters.charAt(random.nextInt(allPossibleCharacters.length())));
         }
-        
-        System.out.println("Generated password: " + password.toString());
-        
-
-        return password.toString();
+    
+        char[] passwordChars = password.toString().toCharArray();
+        for (int i = 0; i < passwordChars.length; i++) {
+            int randomIndex = random.nextInt(passwordChars.length);
+            char temp = passwordChars[i];
+            passwordChars[i] = passwordChars[randomIndex];
+            passwordChars[randomIndex] = temp;
+        }
+    
+        String finalPassword = new String(passwordChars);
+        System.out.println("Generated password: " + finalPassword);
+    
+        return finalPassword;
     }
 
     private String getName() {
-        String line = IoHandler.extractRandomLine("boysNames.csv");
+        String line = IoHandler.extractRandomLine("commonnames.csv");
         if (line != null) {
             String[] userArray = line.split(",");
             String name = userArray[1];
