2d41aa24f7c0ff8d437473415cab2d27f875e0c2
I6356800 <v.rallis@student.maastrichtuniversity.nl>
2024-03-16 12:57:58 +0100

    Added some helper methods to clean up code and in the proccess of implementing the reset password feature.

--- src/User.java
+++ src/User.java
@@ -3,47 +3,29 @@ import java.util.regex.Pattern;
 
 public class User {
 
-    private static String filePath = "users.csv";
+    private static final String FILE_PATH = "users.csv";
 
     public static void main(String[] args) throws NoSuchAlgorithmException {
         loginUser("bill2","Bill!1llllllll");
+        registerUser("bill5","Bill!5*");
+        
     }
 
     // store values in a file
     private static void storeUser(String username, String hashedPassword, String salt) {
-        IoHandler.createFile(filePath);
+        IoHandler.createFile(FILE_PATH);
         String user = username + "," + hashedPassword + "," + salt;
-        IoHandler.writeToFile(filePath, user);
+        IoHandler.writeToFile(FILE_PATH, user);
     }
 
     // register user
     public static void registerUser(String username, String password) throws NoSuchAlgorithmException {
         // check that password has at least 1 number and 1 special character and is at
         // least 8 characters long
-        boolean hasNumber = Pattern.compile("[0-9]").matcher(password).find();
-        boolean hasSpecialChar = Pattern.compile("[^a-zA-Z0-9]").matcher(password).find();
-        boolean isLongEnough = password.length() >= 8;
         boolean hasSpace = Pattern.compile(" ").matcher(username).find();
 
-        if (hasSpace) {
-            System.out.println("Username is not valid");
-            return;
-        }
-
-        if (hasNumber && hasSpecialChar && isLongEnough) {
-            //System.out.println("Password is valid");
-        } else {
-            //System.out.println("Password is not valid");
-            return;
-        }
-
-        if (IoHandler.searchUser(filePath, username) == true) {
-            //System.out.println("Username already exists.");
-            return;
-        }
-        // check that username is at least 4 characters long
-        if (username.length() < 3) {
-            //System.out.println("Username is needs to be at least 4 characters long.");
+        if (checkUsernameValidity(username) == false || checkPasswordValidity(password) == false || hasSpace == true) {
+            System.out.println("Username is invalid.");
             return;
         }
 
@@ -55,7 +37,7 @@ public class User {
 
     // login user
     public static boolean loginUser(String username, String password) throws NoSuchAlgorithmException {
-        String user = IoHandler.searchAndReturnLine(filePath, username);
+        String user = IoHandler.searchAndReturnLine(FILE_PATH, username);
         if (user != null) {
             String[] userArray = user.split(",");
             String salt = userArray[2];
@@ -68,4 +50,45 @@ public class User {
         System.out.println("Login failed");
         return false;
     }
+
+    public static boolean resetForgottenPassword(String username, String newPassword) throws NoSuchAlgorithmException {
+        if (checkUsernameValidity(username)==false || checkPasswordValidity(newPassword)==false) {
+            return false;
+        }
+
+        IoHandler.
+
+
+        String user = IoHandler.searchAndReturnLine(FILE_PATH, username);
+        if (user!=null) {
+            String[] userArray = user.split(",");
+            String salt = userArray[2];
+            String hashedPassword = Hash.hashAndSalt(newPassword, salt);
+            String newUser = username + "," + hashedPassword + "," + salt;
+            IoHandler.replaceLine(FILE_PATH, user, newUser);
+            return true;
+        }
+    }
+
+    public static boolean checkUsernameValidity(String username) {
+        boolean hasSpace = Pattern.compile(" ").matcher(username).find();
+        if (username.length() <= 3 || IoHandler.searchUser(FILE_PATH, username) == true || hasSpace == true) {
+            return false;
+        }
+        return true;
+
+    }
+
+    public static boolean checkPasswordValidity(String password) {
+        boolean hasNumber = Pattern.compile("[0-9]").matcher(password).find();
+        boolean hasSpecialChar = Pattern.compile("[^a-zA-Z0-9]").matcher(password).find();
+        boolean isLongEnough = password.length() >= 8;
+        boolean hasSpace = Pattern.compile(" ").matcher(password).find();
+        
+        if (hasNumber && hasSpecialChar && isLongEnough && !hasSpace) {
+            return true;
+        }
+        return false;
+    }
+
 }
